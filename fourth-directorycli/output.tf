9331dfcc00c20bcc83ca10aae7799d0e79ab75e5
Adedamola <Adedamolamalik@gmail.com>
2025-02-28 14:01:14 +0000

    module 1

--- fourth-directorycli/output.tf
+++
@@ -1,9 +0,0 @@
-output "associate_public_ip_address" {
-  description = "public_ip_address"
-  value = aws_instance.first_vm.public_ip
-}
-
-output "public_dns" {
-  description = "public_dns"
-  value = aws_instance.first_vm.public_dns
-}
